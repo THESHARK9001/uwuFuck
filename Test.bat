d5be2e79f2c2a81e2d1f9c805863273ab1dc3415
THESHARK9001 <76677889+THESHARK9001@users.noreply.github.com>
2025-03-18 13:55:25 -0700

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,6 +1,6 @@
 @echo Please run this AFTER you build/download the exe binary!
 @pause
 
-uwuFuck_FileInterpreter helloworld.txt
-uwuFuck_FileInterpreter helloworld.uf
+uwuFuck_Interpreter helloworld.txt
+uwuFuck_Interpreter helloworld.uf
 @pause
